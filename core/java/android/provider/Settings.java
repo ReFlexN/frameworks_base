40121a4f0356fe483618407fa950ef49341b3c08
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Change-Id: I2a6c5eed646c4a2121890201de2a7b8cca7f1b1a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6612,6 +6612,17 @@ public final class Settings {
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
         /**
+         * Beginning of ReFlex Secure Settings Additions
+         * @hide
+         */
+
+        /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
